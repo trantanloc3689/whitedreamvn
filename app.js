a90691c446d30682fe17ed0470b97e2351e545fa
TranTanLoc <trantanloc3689@gmail.com>
2019-03-25 22:29:11 +0700

    ok
    ok

--- app.js
+++ app.js
@@ -93,7 +93,7 @@ app.use(function(req, res, next) {
   // var err = new Error('Not Found');
   // err.status = 404;
   // next(err);
-  res.redirect('/');
+  res.render('404');
 });
 
 // error handler
